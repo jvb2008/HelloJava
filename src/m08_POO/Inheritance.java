849eaaa5af0ce70237e2509bb6eac223818ef00c
jvb2008 <jmotoso001@gmail.com>
2025-11-03 18:52:35 +0100

    Added polymorphism

---
+++ src/m08_POO/Inheritance.java
@@ -0,0 +1,10 @@
+package m08_POO;
+
+public class Herencia {
+    static void main() {
+        var a = new Persona();
+        a.edad = 39;
+        a.nombre = "Josu";
+        a.hablar();
+    }
+}
